d09ec73dd6f5875f09edf961c47d80f54962b178
tan.jian <tan-jian@foxmail.com>
2017-03-24 16:12:11 +0800

    aaa

--- git-demo-tj/src/main/java/git_demo_tj/git_demo_tj/App.java
+++ git-demo-tj/src/main/java/git_demo_tj/git_demo_tj/App.java
@@ -9,11 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-<<<<<<< HEAD
         System.out.println( "Hello Worldddd!" );
-        System.out.println( "Hello Worltanjian!" );
-=======
-        System.out.println( "Hello Worlddddd" );
->>>>>>> branch 'master' of https://github.com/tanjianj/tj-demo.git
     }
 }
